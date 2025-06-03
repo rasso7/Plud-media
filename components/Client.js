c6d8276afca4112cc4fc71cf585205de2c35ae4c
rashid <mohdrashit15@gmail.com>
2025-06-03 20:41:08 +0530

    with scroll button

--- components/Client.js
+++ components/Client.js
@@ -223,17 +223,7 @@ export default function InfluencerShowcase() {
       </div>
       
       {/* Scroll to top button */}
-      <div className="fixed bottom-6 right-6 z-50">
-        <button 
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-          className="p-3 bg-red-500 text-white rounded-full shadow-lg hover:bg-red-600 transform hover:scale-110 transition-all duration-300"
-          aria-label="Scroll to top"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
-          </svg>
-        </button>
-      </div>
+      
     </section>
   );
 }
